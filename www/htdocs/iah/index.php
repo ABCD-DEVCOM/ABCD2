8b288422f77301427ec11b7eb7d2c3fc0bf8273e
EdS <edsz14@yahoo.com>
2017-11-09 14:14:58 +0100

    automatic configuration for Site and iAH, installation instructions added; small bug removed in ahlist.pft for iAH and new version added in other languages

--- www/htdocs/iah/index.php
+++ www/htdocs/iah/index.php
@@ -1,58 +1,47 @@
 <?php
-$base = ($_REQUEST['base'] != '' ?  $_REQUEST['base'] : 'rda');
+$base = ($_REQUEST['base'] != '' ?  $_REQUEST['base'] : 'marc');
 $lang = ($_REQUEST['lang'] != '' ?  $_REQUEST['lang'] : 'en');
 $form = $_REQUEST['form'];
 
+//define database path according to OS
+if (stripos($_SERVER["SERVER_SOFTWARE"],"Win") > 0)    {
+ $db_path="/ABCD/www/bases/" ;
+ $iah_ver="-win";
+ $ext_exec=".exe";
+ }
+else {
+ $db_path="/var/opt/ABCD/bases/";
+ $iah_ver="-lin";
+ $ext_exec="";
+ }
+//unicode defined in abcd.def
+$dbpath=$db_path."abcd.def";
+$def2= parse_ini_file($dbpath);
+if (isset($def2["UNICODE"]))  {
+ $unicode=trim($def2["UNICODE"]);
+ if (intval($unicode)!=0) $unicode="utf8"; else $unicode="ansi";}
+else
+ $unicode="ansi";
 
-if (stripos($_SERVER["SERVER_SOFTWARE"],"Win") > 0) {
-    //Windows path variables
-$db_path="/ABCD/www/bases/";                   //path where the bases are to be located
-}else{
-    //Linux path variables
-$db_path="/var/opt/ABCD/bases/";
-}
-
-//buscando cisis_ver en dr_path.def de la BD
-$drpathtext=$fp=file($db_path.$base."/dr_path.def");
-foreach($fp as $line)
-{
-	$pos = strpos($line, "CISIS_VERSION");
-	if ($pos !== false)
-	 {
-		$str_line=explode("=",$line);
-		$CISIS_VERSION=$str_line[1];
-	 }
-}
-if($CISIS_VERSION!="")
-{
-	$CISIS_VERSION=trim($CISIS_VERSION);
-	}
-//READING abcd.def to find unicode-setting
-//$fp=file($db_path."par/".strtoupper($base).".def");
-$fp=file($db_path."abcd.def");
-$unicode="ansi";
-foreach($fp as $line)
-{
-	$pos = strpos($line, "UNICODE");
-	if ($pos !== false)
-	 {
-		$str_line=explode("=",$line);
-		$use_unicode=$str_line[1];
-		if($use_unicode!=0)
-{
-	$unicode="utf8";
-}
-else $unicode="ansi";
-}
-}
-//echo "ENCODING=$unicode<BR>";
-if($CISIS_VERSION!="")
-$cisis_ver=$unicode."/".$CISIS_VERSION."/";
+//cisis_ver and unicode defined in dr_path.def
+$drpath= $db_path.$base."/dr_path.def";
+ $def= parse_ini_file($drpath);
+if (isset($def["CISIS_VERSION"]))
+ $cisis_version=trim($def["CISIS_VERSION"]);
+else
+ $cisis_version="";
+if (isset($def["UNICODE"]))  {
+ $unicode=trim($def["UNICODE"]);
+ if (intval($unicode)!=0) $unicode="utf8"; else $unicode="ansi";}
+//echo "cisisver=".$cisis_version."<BR>";
+//die;
+if ($cisis_version!="")
+$cisis_ver=$unicode."/".$cisis_version."/";
 else $cisis_ver=$unicode."/";
-//end
-//Path to the wwwisis executable (include the name of the program, in Windows add .exe)
-$Wxis="$cisis_ver"."wxis";
 
-$hdr = "Location: /cgi-bin/". $Wxis . "/iah/scripts/?IsisScript=iah.xis&lang=" . $lang . "&base=" . strtoupper($base);
+
+//Path to the wwwisis executable (include the name of the program, in Windows add .exe)
+$Wxis=$cisis_ver."wxis" . $ext_exec;
+$hdr = "Location: /cgi-bin/". $Wxis . "/iah/scripts/?IsisScript=iah". $iah_ver . ".xis&lang=" . $lang . "&base=" . strtoupper($base);
 header($hdr);
 ?>
