844e721f59aa411edf86a34e49e98fac2cce04bd
EdS <edsz14@yahoo.com>
2018-05-31 13:59:05 +0200

    directIsis.php updated for Secs-Web

--- www/htdocs/isisws/directIsis.php
+++ www/htdocs/isisws/directIsis.php
@@ -67,30 +67,135 @@ function IsisSearchSort($parametros)
 $wxis_host = $_SERVER['HTTP_HOST'];
 if (stripos($_SERVER["SERVER_SOFTWARE"],"Win")>0) {
 $wxis_action = "/cgi-bin/ansi/wxis.exe";
+include("/ABCD/www/htdocs/central/config.php");
 }else{
 $wxis_action = "/cgi-bin/ansi/wxis";
+include("/opt/ABCD/www/htdocs/central/config.php");
 }
+
 /*
  * This function call through fsockopen function an host that return the xml
  * file with the data you ask.
  * @param $url is mounted by wxis_url function
  
  */
+ function file_get_contents_mbs($filename)
+    {
+       $fh = fopen($filename,'r');
+       
+        clearstatcache();
+        
+            $data = '';
+            while (!feof($fh)) {
+                $data .= fread($fh, 8192);
+            }
+        
+
+        fclose($fh);
+        return $data;
+    }
  function wxis_document_post( $url, $content = "" )
+
 {
-// echo "URL=$url<BR>";
-// echo "content=$content<BR>";
-// die;
-// foreach($_POST as $aux => $array){
-               // foreach($array as $campo => $valor)
-                  // echo "$campo -> $valor <br>";
-             // }
-             // d
-if (stripos($_SERVER["SERVER_SOFTWARE"],"Win") == 0)    //Linux
+global $wxis_action, $db_path, $Wxis;
+
+$tmp=explode("&gizmo",$url);
+$url=$tmp[0];
+if(strpos($url,"write.xis"))
 {
-  $result=file_get_contents($url);
-//  return strstr($result,"<");
-} else { //Windows
+//include("/opt/ABCD/www/htdocs/central/config.php");	
+//include("/ABCD/www/htdocs/central/config.php");
+$content_aux=str_replace("<field ","<field action=add ",$content);
+$content_aux=str_replace("<occ>","",$content_aux);
+$content_aux=str_replace("</occ>","",$content_aux);
+$field=$_POST["field"];
+//foreach($field as $key =>$value) {echo $key."->".$value."<br/>";}exit;
+$db=strtolower ($field["dataBase"]);
+if($db=="") $db=strtolower ($field["database"]);//in mask
+if($db=="") $db=$_GET["m"];
+if($db=="titleplus" || $db=="holdings" || $db=="facic")
+{
+	if($db=="titleplus") $db="titlePlus";
+	$center="main/";
+	$db=$center.$db;
+}
+$mfn=$_POST["mfn"];
+if(!intval($mfn)){//user is creating new record
+$str1="<IsisScript name=new>
+<parm name=cipar><pft>'$db.*=".$db_path."secs-web/$db.*',/
+'htm.pft=$db.pft'</pft></parm>
+<do task=update>
+<parm name=db>$db</parm>
+<parm name=fst><pft>cat('$db.fst')</pft></parm>
+<parm name=mfn>New</parm>
+<field action=define tag=1102>Isis_Status</field>
+<update>";
+$str2="<write>Unlock</write>
+<display>
+<pft>if val(v1102) = 0 then '<b>Created!</b><hr>' fi </pft>
+<pft>if val(v1102) = 1 then '<b>Sorry, no registries created!</b><hr>' fi </pft>
+</display>
+</update>
+</do>
+</IsisScript>";
+
+}
+else if(intval($mfn))//editing
+{
+$str1="<IsisScript name=edit>
+			<parm name=cipar><pft>'$db.*=".$db_path."secs-web/$db.*',/
+'htm.pft=$db.pft'</pft></parm>
+			<do task=update>
+<parm name=db>".$db_path."secs-web/$db</parm>
+<parm name=lockid>aswerty</parm>
+<parm name=mfn>$mfn</parm>
+<parm name=fst><pft>cat('".$db_path."secs-web/$db.fst')</pft></parm>
+<field action=define tag=1101>Isis_Lock</field>
+<field action=define tag=1102>Isis_Status</field>
+<update>
+<write>Lock</write>";
+$itag=1;
+$delete_tag="";
+//while($itag<=9999)
+//{
+//	$delete_tag.="<field action=delete tag=$itag>all</field>";$itag++;
+//}
+$delete_tag.="<proc><pft>,'d*'</pft></proc>";
+$content_aux=$delete_tag.$content_aux;
+$str2="<write>Unlock</write>
+<display><pft>if val(v1102)=0 then
+'<b>Update successful!</b>' fi</pft></display>
+</update>
+</do>
+</IsisScript>";	
+}
+$str=$str1.$content_aux.$str2;
+$new_name="new_".time().".xis";
+//$new_name="new.xis";
+
+//echo "script=$db_path" , "secs-web/$new_name<BR>";
+@ $fp = fopen($db_path."secs-web/$new_name", "w");
+@  flock($fp, 2);
+   fwrite($fp, $str);
+   fclose($fp);
+   $IsisScript=$Wxis." IsisScript=".$db_path."secs-web/$new_name";
+   exec($IsisScript,$out,$b);
+      $execScript=$IsisScript;
+//error_log("execScript = $execScript --- $db_path\n\r",3,'\ABCD\www\bases\log\error.log');
+//   unlink($db_path."secs-web/$new_name");
+    return;
+}
+	$result=file_get_contents_mbs($url);
+	
+	
+     return strstr($result,"<");
+}
+
+function wxis_document_post2( $url, $content = "" )
+{
+
+	$tmp=explode("&gizmo",$url);
+$url=$tmp[0];
     $content = str_replace("\\\"","\"",$content);
     $content = str_replace("\n","",$content);
     $content = str_replace("\r","",$content);
@@ -121,7 +226,7 @@ if (stripos($_SERVER["SERVER_SOFTWARE"],"Win") == 0)    //Linux
     if(function_exists("fsockopen")){
         $fp = fsockopen($host, $port, $errno, $errstr, $timeout);
     }else{
-        print "Function fsockopen must be enabled in file PHP.INI, therefore set allow_url_fopen = On\n";
+        print "Function fsockopen must be enable in file PHP.INI, set allow_url_fopen = On\n";
     }
     // Open the connection to the host, using socket
     //$fp = fopen($url, "r");
@@ -139,8 +244,10 @@ if (stripos($_SERVER["SERVER_SOFTWARE"],"Win") == 0)    //Linux
                 $result .= fgets($fp, 4096);
         }
     }
-   }
+	
     return strstr($result,"<");
+	
+	
 }
 
 /*
