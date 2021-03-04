2a7c8ee8413c55414151818c913fa566b7e69871
Fred <fred.hommersom@xs4all.nl>
2021-03-04 22:17:32 +0100

    Improve code of inverted file generation
    Replaced helper div code by include. Reorder/rewrite code for more readability and correct html output. The "Test" button has now a parameter for the correct mx executable. Solves erroneous exe if multiple database folders are used.
    Deleted file not used: found while browsing.

--- www/htdocs/central/utilities/mx_test.php
+++ www/htdocs/central/utilities/mx_test.php
@@ -1,7 +1,15 @@
 <?php
+/* Modifications
+2021-03-04 fho4abcd Get mx_path from url (required if more database sets are present)
+*/
 include("../config.php");
 echo "<hr>";
-echo "Testing if the  file <b>$mx_path</b> exists<br>";
+if (isset($_GET['mx_path'])) {
+    $mx_path = $_GET['mx_path'];
+}else {
+    echo "<font color=red>Variable 'mx_path' not in the URL. Using default for this test</font><br><br>";
+}
+echo "Testing if the file <b>$mx_path</b> exists<br>";
 if  (!file_exists($mx_path)){
 	echo "<font color=red>missing $mx_path</font>";
 	die;
