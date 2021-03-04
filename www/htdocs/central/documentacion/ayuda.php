261b1fa7588bd70c78e07ebcf44a10db180aded0
Fred <fred.hommersom@xs4all.nl>
2021-03-04 22:19:54 +0100

    Update ayuda.php
    Help files are converted to UTF-8 if necessary. Effect is that all help files display correct now. An error message is added if nothing is found

--- www/htdocs/central/documentacion/ayuda.php
+++ www/htdocs/central/documentacion/ayuda.php
@@ -1,4 +1,8 @@
 <?php
+/* Modifications
+2021-03-03 fho4abcd Display ansi helpfiles with extended characterset correct
+2021-03-03 fho4abcd Display error if not found
+*/
 session_start();
 if (!isset($_SESSION["permiso"])) die;
 include("../common/get_post.php");
@@ -17,7 +21,16 @@ if (file_exists($a)){
 	if (file_exists($a)) $fp=file($a);
 
 }
-if (isset($fp))
-	foreach ($fp as $value) {
-		echo "$value\n";	}
+if (isset($fp)) {
+    $content = file_get_contents($a);
+    $homepage= mb_convert_encoding($content, 'UTF-8', mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true));
+} else {
+    $homepage= "<p>Help file &#8594; ".$arrHttp["help"]." &#8592; Does not exist.<br></p>";
+    $homepage= $homepage."<p>We cannot find a suitable replacement too. So sorry</p>";
+}
+?>
+<!DOCTYPE html>
+<html><body>
+<?php
+echo $homepage;
 ?>
