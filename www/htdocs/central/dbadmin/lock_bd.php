ca7073c10bcff44864de22ddde8aa18bf8e6124c
Fred <fred.hommersom@xs4all.nl>
2021-04-12 19:02:24 +0200

    Improve function Protect database
    - lock_bd.php: Redesigned. Improved helper fragment+ breadcrumb+show actual status+better menu+correct html+...
    - Orphans VMX.php deleted. (not used at all)

--- www/htdocs/central/dbadmin/lock_bd.php
+++ www/htdocs/central/dbadmin/lock_bd.php
@@ -1,118 +1,115 @@
 <?php
+/* Modifications
+20210413 fho4abcd Redesign. Improved:helper fragment+breadcrumb+feedback+simplified coding,...
+*/
 session_start();
-include("../common/get_post.php");
-include ("../config.php");
-if (!isset($_SESSION["permiso"]) or !isset($arrHttp["base"])){
+if (!isset($_SESSION["permiso"])){
 	header("Location: ../common/error_page.php") ;
 }
-
-include("../lang/dbadmin.php");
-include("../lang/soporte.php");
-include("../lang/admin.php");
+if (!isset($_SESSION["lang"]))  $_SESSION["lang"]="en";
+include("../common/get_post.php");
+include ("../config.php");
 $lang=$_SESSION["lang"];
-//foreach ($arrHttp as $var=>$value) echo "$var=$value<br>";
-
-function Ejecutar($db,$action,$db_path){
-	$filename=$db_path.$db."/protect_status.def";
-	switch($action){		case "protect_db":
-			if (!$handle = fopen($filename, 'w')) {
-	         	echo "Cannot open file ($filename)";
-	         	return -1;
-			 	exit;
-	   		}
-	   		$contenido="PROTECTED";
-			// Write $somecontent to our opened file.
-			if (fwrite($handle, $contenido) === FALSE) {
-	       		echo "Cannot write to file ($filename)";
-		   		return -1;
-	       		exit;
-	   		}
-	   		fclose($handle);
-	   		return 0;
-   			break;
-   		case "unprotect_db":
-   			$res=0;
-   			if (file_exists($filename)){
-   				$res=unlink($filename);
-   			}
-   			return 0 ;
-            break;	}
-
-
-
-
-	$ix=strlen($db_path);
-
-
-   return 0;
-
-}
-
-
-//foreach ($arrHttp as $var=>$value) echo "$var=$value<br>";
 include("../common/header.php");
-?>
-<script>
-function EnviarForma(){	seleccion=""	for(ix=0;ix<document.protect.action.length;ix++){		if (document.protect.action[ix].checked)
-			seleccion="Y"
-	}
-	if (seleccion==""){		return	}
-	document.protect.submit()}
-</script>
-<?php
-echo "<body>\n";
+include("../lang/dbadmin.php");
+include("../lang/admin.php");
+
+$backtoscript="../dbadmin/menu_mantenimiento.php";
+$backtourl=$backtoscript."?base=".$arrHttp["base"];
+$base=$arrHttp["base"];
+$protectfilename=$db_path.$base."/protect_status.def";
+if (file_exists($protectfilename)) {
+    $protectstatus=1;
+    $protectstatusmsg="Protected";
+} else {
+    $protectstatus=0;
+    $protectstatusmsg="NOT protected";
+}
+echo "<body>";
+// Show institutional info
 include("../common/institutional_info.php");
 ?>
 <div class="sectionInfo">
-
-			<div class="breadcrumb">
-				<?php echo "<h5>".$msgstr["protect_db"]." " .$msgstr["database"].": ".$arrHttp["base"]."</h5>"?>
-			</div>
-
-			<div class="actions">
-<?php
-	if (!isset($arrHttp["eliminar"]))
-    	echo "<a href=\"menu_mantenimiento.php?base=".$arrHttp["base"]."&encabezado=S\" class=\"defaultButton backButton\">";
-	else
-		echo "<a href=\"../common/inicio.php?reinicio=s\" class=\"defaultButton backButton\">";
-
-?>
-					<img src="../images/defaultButton_iconBorder.gif" alt="" title="" />
-					<span><strong><?php echo $msgstr["back"]?></strong></span>
-				</a>
-			</div>
-			<div class="spacer">&#160;</div>
+	<div class="breadcrumb">
+        <?php echo $msgstr["maintenance"]." &rarr; ".$msgstr["protect_db"];?>
+	</div>
+	<div class="actions">
+        <a href='<?php echo $backtourl?>'  class="defaultButton backButton">
+		<img src="../images/defaultButton_iconBorder.gif" alt="" title="" />
+		<span><strong><?php echo $msgstr["regresar"]?></strong></span></a>
+	</div>
+	<div class="spacer">&#160;</div>
 </div>
+<?php include "../common/inc_div-helper.php" ?>
+<div class="middle form">
+	<div class="formContent">
+    <div align=center>
+        <h3><?php echo $msgstr["protect_db"]?></h3>
+    </div>
 <?php
-echo "<div class=helper>
-<font size=1>Script: dbadmin/lock_bd.php</font>
-</div>
-<div class=\"middle form\">
-			<div class=\"formContent\">
-
-";
-echo "<center><h3>".$msgstr["protect_db"]."</h3></center>";
-if (!isset($_SESSION["permiso"]["CENTRAL_ALL"])){	echo "<h4>".$msgstr["invalidright"]."</h4>";}else{
-	if (!isset($arrHttp["action"])){		echo "<form name=protect onsubmit='return false'><table align=center class=listTable>";
-		echo "<input type=hidden name=base value=".$arrHttp["base"].">\n";
-		echo "<tr><td><input type=radio name=action value=protect_db>".$msgstr["protect_db"]."</td></tr>\n";
-		echo "<tr><td><input type=radio name=action value=unprotect_db>".$msgstr["unprotect_db"]."</td></tr>\n";
-		echo "<tr><td><input type=submit name=send value=".$msgstr["send"]." onclick=EnviarForma()></td></tr>\n";
-		echo "</table></form>";
-	}else{		$res=Ejecutar($arrHttp["base"],$arrHttp["action"],$db_path);
-		echo "<h4>".$arrHttp["base"].": ".$msgstr[$arrHttp["action"]];
-		switch ($res){			case 0:
-				echo " OK";
-				break;
-			case -1:		       echo " FAILED!!!";
-		       break;
-		}
-	    echo "</h4>";	}
+if (!isset($_SESSION["permiso"]["CENTRAL_ALL"])){
+	echo "<h4>".$msgstr["invalidright"]."</h4>";die;
 }
-echo "</div>
+if (!isset($arrHttp["action"])){
+    ?>
+    <div align=center>
+        <b><?php echo $msgstr["bd"].": ".$arrHttp["base"]." &rarr; ".$protectstatusmsg ?></b><br><br>
+    </div>
+    <form name=protect method=post>
+        <input type=hidden name=base value=<?php echo $arrHttp["base"]?>>
+        <table align=center>
+            <?php if ($protectstatus==0) {?>
+            <tr><td><input type=checkbox name=action value=protect_db></td>
+                <td><?php echo $msgstr["protect_db"]?></td>
+            </tr>
+            <?php } else { ?>
+            <tr><td><input type=checkbox name=action value=unprotect_db></td>
+                <td><?php echo $msgstr["unprotect_db"]?></td>
+            </tr>
+            <?php } ?>
+            <tr><td></td><td><input type=submit  value=<?php echo $msgstr["procesar"]?> ></td></tr>
+        </table>
+    </form>
+    </div>
 </div>
-";
-include("../common/footer.php");
-echo "</body></html>\n";
-
+<?php
+}else{
+    $execstatus="??";$action="none";
+    if (isset($_POST['action'])) $action=$_POST['action'];
+    if ($action=="protect_db") {
+        if (!$handle = fopen($protectfilename, 'w')) {
+            $execstatus=" FAILED!!! &rarr; Cannot open file ".$protectfilename;
+        }        
+        if ( $handle and fwrite($handle, "PROTECTED") === FALSE) {
+            $execstatus=" FAILED!!! &rarr; Cannot write to file ".$protectfilename;
+        }
+        if ( $handle and $execstatus=="??") {
+            fclose($handle);
+            $execstatus= " OK";
+        }
+    } else if ($action=="unprotect_db") {
+        $execstatus= " OK";
+        if (file_exists($protectfilename)){
+            if (!unlink($protectfilename)) {
+                $execstatus=" FAILED!!!";
+            }
+        }
+    }
+    if (file_exists($protectfilename)) {
+        $protectstatusmsg="Protected";
+    } else {
+        $protectstatusmsg="NOT protected";
+    }
 ?>
+    <div align=center>
+        <h4><?php echo $arrHttp["base"].": ".$msgstr[$arrHttp["action"]]." &rarr; ".$execstatus?></h4>
+         <b><?php echo $msgstr["bd"].": ".$arrHttp["base"]." &rarr; ".$protectstatusmsg ?></b>
+    </div>
+</div>
+</div>
+<?php
+}
+include("../common/footer.php");
+?>
+</body></html>
+
