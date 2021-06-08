0ea5fb5c1958fe8aa5cfd2166da02942f5e6c220
Fred <fred.hommersom@xs4all.nl>
2021-06-08 18:48:57 +0200

    Add "Utilities->"Convert ISO <-> UTF-8"--> "Convert Iso file to UTF-8"
    - vmx_import.php : The part to show the content of the wrk folder is moved to
    - inc_show_work.php: A reusable piece of code to show the content of the wrk folder and action icons.
    - menu-bar.php : extended with the new menu item
    - cnv_iso_2_utf.php: code to select an iso file and send it to conversion program cgi-bin/isofile_iso_to_utf.
    - test cnvrt.php: code to check existence and minimal operation of cgi-bin/isofile_iso_to_utf

---
+++ www/htdocs/central/utilities/test_cnvrt.php
@@ -0,0 +1,32 @@
+<?php
+/* Modifications
+20210608 fho4abcd Created
+*/
+include("../config.php");
+echo "<hr>";
+if (isset($_GET['cnvexe'])) {
+    $cnvexe = $_GET['cnvexe'];
+}else {
+    echo "<font color=red>Variable 'cnvexe' not in the URL.</font>";
+    die;
+}
+echo "Testing if the file <b>$cnvexe</b> exists<br>";
+if  (!file_exists($cnvexe)){
+	echo "<font color=red>missing $cnvexe</font>";
+	die;
+}else{
+	echo "<strong>OK!!</strong>";
+}
+echo "<p><font color=blue>Testing the execution of  <b>$cnvexe</b></font><br>";
+$strINV=$cnvexe." -h 2>&1";
+echo "<strong>Command line: $strINV</STRONG>";
+echo "<div style='font-family:courier;font-size:12px'>";
+exec($strINV, $output,$t);
+$straux="";
+for($i=0;$i<count($output);$i++)
+{
+echo $output[$i]."<br>";
+}
+echo "</div>";
+
+?>
