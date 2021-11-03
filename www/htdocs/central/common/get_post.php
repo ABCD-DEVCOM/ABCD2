3fdee18915f39e460d25e699e44d68e1027540f2
Fred <fred.hommersom@xs4all.nl>
2021-11-03 17:05:16 +0100

    Improve fullinv.php
    This commit  enhances the Inverted File Generation script by:
    - The fixed field for  digital document html file is replaced by the field specified in the collection data configuration
    - The FDT is scanned for fields with an HTML Area. The gizmo can also be applied to these fields.
    - All combinations are supported (e.g.: no dig docs+no fdt fields-> dig docs + N fdt fields)
    - The form is simplified: Shows field selection boxes  to apply/omit HTML tag processing. Only if these fields are present. Default is set to "strip". The checkbox to enable html tag processing is removed as the new selection boxes give finer control.
    - A suggestion is shown if the gizmo cannot be processed by the mx associated with the database
    Note: The option for the "m" parameter is still present, but default "off".

--- www/htdocs/central/common/get_post.php
+++ www/htdocs/central/common/get_post.php
@@ -1,17 +1,11 @@
 <?php
+/*
+20211102 fho4abcd Check for array variables (will not be entered in $arrHttp)
+*/
 if (!isset($SESION["super_user"]))  $_SESSION["super_user"]="";
 $arrHttp=array();
 //include("validar_request.php");
 foreach ($_REQUEST as $var => $value) {
-	if (trim($value)!="") $arrHttp[$var]=$value;
+	if ( !is_array($value) AND trim($value)!="") $arrHttp[$var]=$value;
 }
-
-//$fp=fopen("/abcd/www/scripts","a");
-//fwrite($fp,$_SERVER["PHP_SELF"]."\n");
-//fclose($fp);
-//foreach ($arrHttp as $var => $value) 	echo "$var = $value<br>";
-
-
-
-
 ?>
