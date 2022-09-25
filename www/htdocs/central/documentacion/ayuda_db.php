5c3ba701de3649626d50af5887262d69d58c7db8
Fred <fred.hommersom@xs4all.nl>
2022-09-25 19:45:31 +0200

    Update edit of field tag help files
    - Solved bug: an edited file is now correctly saved
    - Folder part ayudas/<lang> is created if it does not exist.
    - New style buttons
    - File help-ed.php is moved to folder documentacion
    - Many fixed strings are now translated. Some translations improved

--- www/htdocs/central/documentacion/ayuda_db.php
+++ www/htdocs/central/documentacion/ayuda_db.php
@@ -1,32 +1,33 @@
 <?php
+/*
+20220925 fho4abcd cleanup+ new style button
+*/
 session_start();
 include("../common/get_post.php");
 include ("../config.php");
 $lang=$_SESSION["lang"];
-include("../lang/soporte.php");
 include("../lang/admin.php");
-include("../lang/lang.php");
 //foreach ($arrHttp as $var=>$value) echo "$var=$value<br>";
 
-if (!isset($_SESSION["permiso"])) {	echo "session expired";
-	die;}
+if (!isset($_SESSION["permiso"])){
+	header("Location: ../common/error_page.php") ;
+}
 unset($fp);
 $archivo=$db_path.$arrHttp["base"]."/ayudas/".$_SESSION["lang"]."/".$arrHttp["campo"];
-
-if (file_exists($archivo)){	$fp=file($archivo);
-}else{	$archivo=$db_path.$arrHttp["base"]."/ayudas/".$lang_db."/".$arrHttp["campo"];
-
-	if (file_exists($archivo)){		$fp=file($archivo);
-	}
+if (!file_exists($archivo)){	$archivo=$db_path.$arrHttp["base"]."/ayudas/".$lang_db."/".$arrHttp["campo"];
 }
-
 if (!file_exists($archivo)){
-	echo "<h4>".$arrHttp["base"]."/ayudas/".$_SESSION["lang"]."/".$arrHttp["campo"] ."</h4>";	echo $msgstr["notfound"];
+    include("../common/header.php");
+	echo "<h4>".$arrHttp["base"]."/ayudas/".$_SESSION["lang"]."/".$arrHttp["campo"]." &rarr; ".$msgstr["notfound"]."</h4>";    ?>
+    <div>
+    <a class="bt bt-red" href="javascript:window.close();"><i class="far fa-window-close"></i> &nbsp;<?php echo $msgstr["cerrar"]?></a>
+    </div>
+    <?php
 	die;}
-if (isset($fp)){	foreach ($fp as $value) {
-		$value=str_replace('/php',$app_path.'/',$value);
-		echo "$value\n";
-	}}
-?>
+$fp=file($archivo);
+foreach ($fp as $value) {
+    $value=str_replace('/php',$app_path.'/',$value);
+    echo "$value\n";
+}?>
 </body>
 </html>
