a593c23274df41fcec7551540d564b1346384e50
Roger C Guilherme <rogercguilherme@gmail.com>
2023-03-14 16:20:46 -0300

    Fixes the LDAP connection
    Fixes the LDAP connection
    
    The connection test was made with the jumpcloud.com platform.
    
    To make it work you need to change the following variables in the config.php file:
    
    $use_ldap=1;
    
    and these more:
    
    $ldap_host = "ldap.jumpcloud.com
    $ldap_dn = "uid=rogercgui,ou=Users,o=12345e63a6932ca94fabcd,dc=jumpcloud,dc=com";
    $ldap_search_context = "uid=rogercgui,ou=Users,o=12345e63a6932ca94fabcd,dc=jumpcloud,dc=com"
    $ldap_port = "389"
    $ldap_pass = "MyPass"

--- www/htdocs/central/common/ldap.php
+++ www/htdocs/central/common/ldap.php
@@ -2,29 +2,34 @@
 /* modifications
 2021-02-25 fh04abcd removed empty line before php tag. (shown before DOCTYPE=non-html). Deleted old comment
 */
-function isLdapUser($username,$password, $ldap){
-        global $ldap_dn,$ldap_pass,$ldap_search_context;
 
-        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION,3);
-        ldap_set_option($ldap, LDAP_OPT_REFERRALS,0);
+ 
+    $ldap_con = ldap_connect($ldap_host);
+    ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+function isLdapUser($username, $ldap){
+        global $ldap_dn,$ldap_pass,$ldap_search_context,$ldap_con;
+
+        ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION,3);
+        ldap_set_option($ldap_con, LDAP_OPT_REFERRALS,0);
         $attrib = array('dn'); 
 
-		$r=ldap_bind($ldap,$ldap_dn,$ldap_pass);
+		$r=@ldap_bind($ldap_con, $ldap_dn, $ldap_pass);
 
-        $ldapget = @ldap_search($ldap,$ldap_search_context,"(uid=$username*)",$attrib);
-        $ldapcat = @ldap_get_entries($ldap,$ldapget);
+        $ldapget = @ldap_search($ldap_con,$ldap_search_context,"(uid=$username*)",$attrib);
+        $ldapcat = @ldap_get_entries($ldap_con,$ldapget);
         $candidato = $ldapcat[0]["dn"];
 
-        if (@ldap_bind($ldap,$candidato,$password))
+        if(@ldap_bind($ldap_con, $ldap_dn, $ldap_pass))
                return true;
 
         return false;
 }
 
 function Auth($username, $password, string $adGroupName = 'false'){
-    global $ldap_host,$ldap_dn,$ldap_port;
+    global $ldap_host, $ldap_con;
 
-    $ldap = ldap_connect($ldap_host,$ldap_port);
+    $ldap = ldap_connect($ldap_host);
 
     if (!$ldap)
         throw new Exception("Cant connect ldap server, Check in your browser that you have connection to the ldap server", 1);
@@ -40,18 +45,18 @@ function Info($username, $password, $attrib, string $adGroupName = 'false'){
     if (!$ldap)
         throw new Exception("Cant connect ldap server", 1);
 
-        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION,3);
-        ldap_set_option($ldap, LDAP_OPT_REFERRALS,0);
+        ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION,3);
+        ldap_set_option($ldap_con, LDAP_OPT_REFERRALS,0);
 
-		$r=ldap_bind($ldap,$ldap_dn,$ldap_pass);
+		$r=@ldap_bind($ldap_con, $ldap_dn, $ldap_pass);
 
-        $ldapget = @ldap_search($ldap,$ldap_search_context,"(uid=$username*)",$attrib);
-        $ldapcat = @ldap_get_entries($ldap,$ldapget);
+        $ldapget = @ldap_search($ldap_con,$ldap_search_context,"(uid=$username*)",$attrib);
+        $ldapcat = @ldap_get_entries($ldap_con,$ldapget);
 	   return $ldapcat;
 }
 
 function Exist($username){
-    global $ldap_host,$ldap_port,$ldap_pass,$ldap_dn,$ldap_search_context;
+    global $ldap_host,$ldap_port,$ldap_pass,$ldap_dn,$ldap_search_context,$ldap_con;
 
     $exist = false;
     $candidato ="";
@@ -59,13 +64,13 @@ function Exist($username){
     if (!$ldap)
         throw new Exception("Cant connect ldap server", 1);
 
-    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION,3);
-    ldap_set_option($ldap, LDAP_OPT_REFERRALS,0);
+    ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION,3);
+    ldap_set_option($ldap_con, LDAP_OPT_REFERRALS,0);
 	$attrib = array('dn');
-    $r=ldap_bind($ldap,$ldap_dn,$ldap_pass);
+    $r=@ldap_bind($ldap_con, $ldap_dn, $ldap_pass);
 
-	 $ldapget = @ldap_search($ldap,$ldap_search_context,"(uid=$username*)",$attrib);
-     $ldapcat = @ldap_get_entries($ldap,$ldapget);
+	 $ldapget = @ldap_search($ldap_con,$ldap_search_context,"(uid=$username*)",$attrib);
+     $ldapcat = @ldap_get_entries($ldap_con,$ldapget);
      $candidato = $ldapcat[0]["dn"];
 	 if($candidato != "")$exist = true;
 
