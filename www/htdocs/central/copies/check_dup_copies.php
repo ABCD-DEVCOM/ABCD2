d790a1a67b1d73c6d3b704cf2aad342d3ae00aaf
Roger C Guilherme <rogercguilherme@gmail.com>
2023-04-24 00:10:46 -0300

    Modifies the execution of the duplicate record check
    Modifies the execution of the duplicate record check script in the Copies database. Now the script no longer uses MX but WXIS.
    
    MX is not running on SSL.

--- www/htdocs/central/copies/check_dup_copies.php
+++ www/htdocs/central/copies/check_dup_copies.php
@@ -1,21 +1,42 @@
 <?php
+/**
+ * 20230424 rogercgui This script was remade to use WXIS and not MX. For some reason not yet understood, MX does not run on SSL-enabled servers.
+ * 
+ * 
+ * 
+ * 
+ */
+
 session_start();
+ini_set('error_reporting', E_ALL);
 include("../config.php");
 $response="";
-$OS=strtoupper(PHP_OS);
-$converter_path=$cisis_path."mx";
-$in=$_GET["invnum"];
-$mx=$converter_path." ".$db_path."copies/data/copies IN_".$in." now";
-exec($mx,$outmx,$banderamx);
-$textoutmx="";
-for ($i = 0; $i < count($outmx); $i++) {
-$textoutmx.=$outmx[$i]."~";
+
+$in=$_REQUEST["invnum"];
+
+$bd_ref="copies";
+
+$Expresion="IN_".$in;
+$IsisScript=$xWxis."buscar.xis";
+$arrHttp["base"]=$bd_ref;
+$arrHttp["cipar"]=$bd_ref.".par";
+$arrHttp["Opcion"]="buscar";
+$Formato="ALL";
+$query = "&cipar=$db_path"."par/".$arrHttp["cipar"]. "&Expresion=$Expresion&Opcion=".$arrHttp["Opcion"]."&base=" .$arrHttp["base"]."&Formato=$Formato&prologo=NNN";
+
+include("../common/wxis_llamar.php");
+
+$i = 0;
+foreach ($contenido as $value){
+	//echo $value."<br>";
+	$splitbymfn=explode("mfn=",$value);
+	$outmx=$value;
+	$conta=(count($splitbymfn)-1)."<br>";
+	if ($conta>="1"){ 
+			echo "1";
+	} else {
+			echo "~";
+	}
 }
-$splitbymfn=explode("mfn=",$textoutmx);
-for ($i = 1; $i < count($splitbymfn); $i++) {//Now going one by one mfn
-$splitbymarc=explode("~",$splitbymfn[$i]);
-$response.=str_replace(" ",'',$splitbymarc[0])."~";
-}//Now going one by one mfn
-if ($response=="") $response="~";
-echo $response;
+
 ?>
