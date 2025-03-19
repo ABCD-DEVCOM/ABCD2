d2a76eaa13dc69149e66ae24f8d5778596857770
Roger C Guilherme <rogercguilherme@gmail.com>
2025-03-19 14:06:25 -0300

    Corrects the EnviarFormaCN function and cleans up the HTML.

--- www/htdocs/central/dbadmin/assign_control_number.php
+++ www/htdocs/central/dbadmin/assign_control_number.php
@@ -1,11 +1,4 @@
 <?php
-/* Modifications
-20210613 fho4abcd remove password, lineends
-20211215 fho4abcd Backbutton by & helper by included file
-20220203 fho4abcd Cleanup code&html+translation+make it work after reinvoke
-20220303 rogercgui Corrected the absence of the variable in the function "EnviarFormaCN"
-20220321 fho4abcd Reinstalled absence of variable. Use new par variable
-*/
 session_start();
 if (!isset($_SESSION["permiso"])){
 	header("Location: ../common/error_page.php") ;
@@ -13,15 +6,36 @@ if (!isset($_SESSION["permiso"])){
 //error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 include("../common/get_post.php");
 include ("../config.php");
-include("../lang/admin.php");
+include ("../lang/admin.php");
 include("../lang/soporte.php");
-include("../lang/dbadmin.php");
+include ("../lang/dbadmin.php");
 
+
+
+
+
+function LeerArchivos($Dir,$Ext){
+// se leen los archivos con la extensión .pft
+$the_array = Array();
+$handle = opendir($Dir);
+while (false !== ($file = readdir($handle))) {
+   if ($file != "." && $file != "..") {
+   		if(is_file($Dir."/".$file))
+		   if (substr($file,strlen($file)-4,4)==".".$Ext) $the_array[]=$file;
+   }
+}
+closedir($handle);
+return $the_array;
+}
 
 // ==================================================================================================
 // INICIO DEL PROGRAMA
 // ==================================================================================================
+
 //
+
+
+
 //foreach ($arrHttp as $var=>$value) echo "$var=$value<br>";
 
 if (isset($arrHttp["encabezado"]))
@@ -32,7 +46,7 @@ else {
 
 //GET THE MAX MFN
 $IsisScript=$xWxis."administrar.xis";
-$query = "&base=".$base . "&cipar=$db_path".$actparfolder.$base.".par&Opcion=status";
+$query = "&base=".$arrHttp["base"] . "&cipar=$db_path".$actparfolder.$arrHttp["base"].".par&Opcion=status";
 
 include("../common/wxis_llamar.php");
 $ix=-1;
@@ -46,9 +60,16 @@ foreach($contenido as $linea) {
 	  	}
 	}
 }
+?>
 
+
+<script>
+Maxmfn=<?php echo $tag["MAXMFN"];?>;
+</script>
+
+<?php
 //GET LAST CONTROL NUMBER
-$archivo=$db_path.$base."/data/control_number.cn";
+$archivo=$db_path.$arrHttp["base"]."/data/control_number.cn";
 if (!file_exists($archivo)){
 	$fp=fopen($archivo,"w");
 	$res=fwrite($fp,"");
@@ -60,11 +81,10 @@ if (!file_exists($archivo)){
 
 include("../common/header.php");
 ?>
-<body>
 <script language="JavaScript" type="text/javascript" src="../dataentry/js/lr_trim.js"></script>
 <script language="JavaScript" type="text/javascript" src="../dataentry/js/selectbox.js"></script>
 
-<script language=javascript>
+<script language="javascript">
 
 function BorrarRango(){
 	document.cnFrm.Mfn.value=''
@@ -72,9 +92,9 @@ function BorrarRango(){
 }
 
 function EnviarFormaCN(vp){
-	de=Trim(document.cnFrm.Mfn.value)
-  	a=Trim(document.cnFrm.to.value)
-  	Maxmfn=<?php echo $tag["MAXMFN"];?>
+	de=Trim(document.cnFrm.Mfn.value);
+  	a=Trim(document.cnFrm.to.value);
+  	Maxmfn=<?php echo $tag["MAXMFN"];?>;
   	if (de!="" || a!="") {
   		Se=""
 		var strValidChars = "0123456789";
@@ -105,6 +125,7 @@ function EnviarFormaCN(vp){
 }
 
 </script>
+
 <?php
 if (isset($arrHttp["encabezado"])){
 	include("../common/institutional_info.php");
@@ -112,7 +133,7 @@ if (isset($arrHttp["encabezado"])){
 ?>
 <div class="sectionInfo">
 	<div class="breadcrumb">
-    <?php echo $msgstr["assigncn"].": ".$base?>
+    <?php echo $msgstr["assigncn"].": ".$arrHttp["base"]?>
 	</div>
 	<div class="actions">
     <?php
@@ -120,8 +141,8 @@ if (isset($arrHttp["encabezado"])){
 	if (isset($arrHttp["encabezado"])){
 		if (isset($_SESSION["permiso"]["CENTRAL_ALL"]) or
             isset($_SESSION["permiso"]["CENTRAL_MODIFYDEF"]) or
-            isset($_SESSION["permiso"][$base."_CENTRAL_MODIFYDEF"]) or
-            isset($_SESSION["permiso"][$base."_CENTRAL_ALL"])){
+            isset($_SESSION["permiso"][$arrHttp["base"]."_CENTRAL_MODIFYDEF"]) or
+            isset($_SESSION["permiso"][$arrHttp["base"]."_CENTRAL_ALL"])){
             $backtoscript="../dbadmin/menu_mantenimiento.php";
 		}else{
             $backtoscript="../common/inicio.php";
@@ -131,77 +152,71 @@ if (isset($arrHttp["encabezado"])){
     </div>
     <div class="spacer">&#160;</div>
 </div>
+
 <?php include "../common/inc_div-helper.php" ?>
 <div class="middle form">
 <div class="formContent">
+
+	<h3><?php echo $msgstr["assigncn"];?></h3>
+
+    <form name="cnFrm" method="post" action="assign_control_number_ex.php" onsubmit="Javascript:return false">
+    <input type="hidden" name="encabezado" value="s">
+    <input type="hidden" name="base" value="<?php echo $arrHttp["base"]?>">
+    <input type="hidden" name="cipar" value="<?php echo $arrHttp["base"]?>.par">
+
+	<p><?php echo $msgstr["r_recsel"]?>
+
+	<b><?php echo $msgstr["r_mfnr"]?>:</b></p>
+	
+	<label><?php echo $msgstr["r_desde"];?></label>
+		<input type="text" name="Mfn" size="10" value="<?php if (isset($arrHttp["from"])) echo $arrHttp["from"]; else echo "1";?>"> 
+	
 <?php
-echo "<center><h3>".$msgstr["assigncn"]."</h3></center>";
-?>
-    <form name=cnFrm method=post action=assign_control_number_ex.php onsubmit="Javascript:return false">
-    <input type=hidden name=encabezado value=s>
-    <input type=hidden name=base value=<?php echo $base?>>
-    <input type=hidden name=cipar value=<?php echo $base?>.par>
-	<table width=100% cellpadding=5>
-	<tr>
-		<td colspan=2 align=center height=1 bgcolor=#eeeeee><?php echo $msgstr["r_recsel"]?></td>
-	<tr>
-		<td  align=center colspan=2><strong><?php echo $msgstr["r_mfnr"]?></strong>: &nbsp; &nbsp; &nbsp;
-		<?php echo $msgstr["r_desde"].": <input type=text name=Mfn size=10 value=";
-		if (isset($arrHttp["from"]))
-			echo $arrHttp["from"];
-		else
-			echo "1";
-		echo ">&nbsp; &nbsp; &nbsp; &nbsp;";
-		echo $msgstr["r_hasta"].": <input type=text name=to size=10 value='";
-		if (isset($arrHttp["to"])){
-			$count=$arrHttp["to"]-$arrHttp["from"]-1;
-			$count= $arrHttp["to"]+$count-1;
-			if ($count>$tag["MAXMFN"])
-				echo $tag["MAXMFN"];
-			else
-				echo $count;
+	if (isset($arrHttp["to"])) { 
+		$count=$arrHttp["to"]-$arrHttp["from"]-1; 
+		$count= $arrHttp["to"]+$count-1; 
+		$to=$count;
+	if ($count>$tag["MAXMFN"]) {
+		$to=$tag["MAXMFN"]; 
+	} else {
+		$to=$count;
 		}
-		echo "'>";
-		echo "&nbsp;".$msgstr["maxmfn"].": ".$tag["MAXMFN"]
-		?>
-		&nbsp; &nbsp; &nbsp; <a href=javascript:BorrarRango() class=boton><?php echo $msgstr["borrar"]?></a>
-	</td>
-	<tr>
-		<td colspan=2 align=center>
-        <input type=submit name=enviar value="<?php echo $msgstr["send"]?>" onClick="javascript:EnviarFormaCN()">
-        <br><br>
-        <?php
-		echo $msgstr["lastcn"].": ".$last_cn."&nbsp; <a href=Javascript:Reset()>".$msgstr["resetcn"]."</a>";
-        ?>
-        </td>
-    </tr>
-</table>
+	} else {
+		$to="";
+	}
+?>
+
+	<label><?php echo $msgstr["r_hasta"];?></label>
+		<input type="text" name="to" size="10" value="<?php echo $to;?>">
+		
+	<small><?php echo $msgstr["maxmfn"].": ".$tag["MAXMFN"];?></small>
+
+	<div class="py-5">
+		<a href="javascript:BorrarRango()" class="bt bt-default">
+			<i class="fa fa-times"></i> <?php echo $msgstr["borrar"]?>
+		</a>
+
+        <button type="submit" class="bt-blue" onClick="javascript:EnviarFormaCN()" name=enviar>
+			<i class="fa fa-step-forward"></i> <?php echo $msgstr["send"]?>
+		</button> 
+		
+        <p><?php echo $msgstr["lastcn"].": ".$last_cn."&nbsp; <a href=Javascript:Reset()>".$msgstr["resetcn"];?></a></p>
+
+	</div>
 </form>
 </div>
 </div>
-<form name=reset_nc method=post action=reset_control_number.php>
-<input type=hidden name=base value=<?php echo $base?>>
-<input type=hidden name=encabezado value=s>
+
+<form name="reset_nc" method="post" action="reset_control_number.php">
+	<input type="hidden" name="base" value="<?php echo $arrHttp["base"]?>">
+	<input type="hidden" name="encabezado" value="s">
 </form>
-<script>function Reset(){
+
+<script>
+function Reset(){
 	document.reset_nc.submit()
 }
 </script>
-<?php
-include("../common/footer.php");
 
-//================= functions =============
-function LeerArchivos($Dir,$Ext){
-// se leen los archivos con la extensión .pft
-$the_array = Array();
-$handle = opendir($Dir);
-while (false !== ($file = readdir($handle))) {
-   if ($file != "." && $file != "..") {
-   		if(is_file($Dir."/".$file))
-		   if (substr($file,strlen($file)-4,4)==".".$Ext) $the_array[]=$file;
-   }
-}
-closedir($handle);
-return $the_array;
-}
-?>
+
+<?php include("../common/footer.php"); ?>
