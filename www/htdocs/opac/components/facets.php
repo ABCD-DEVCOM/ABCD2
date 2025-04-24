b966ab3773c8b6fab9267fccdf56d782ad526173
Roger C Guilherme <rogercguilherme@gmail.com>
2025-04-23 23:50:27 -0300

    New scripts created for a more efficient facet structure.
    From now on, databases must contain a file called basededados_facetas.dat with a structure very similar to the file camposbusqueda.tab.
    
    The difference is that in the column that points to the field you can use a different language.

--- www/htdocs/opac/components/facets.php
+++ www/htdocs/opac/components/facets.php
@@ -1,112 +1,181 @@
-<?php 
+<?php
+
+function facetas()
+{
+    global $db_path, $lang, $msgstr, $actparfolder, $xWxis, $busqueda, $Expresion, $primera_base, $ABCD_scripts_path, $IsisScript, $expresion, $base;
+
+    $facetas = "S";
+
+    include("includes/leer_bases.php");
+
+    if (isset($facetas) and $facetas == "S") {
+        $archivo = "";
 
 
-function facetas(){
-
-global $db_path, $lang, $msgstr, $actparfolder, $xWxis, $busqueda, $Expresion,$primera_base ;
-
-/*
-if (isset($_REQUEST["base"])){
-    $_REQUEST["base"]=$_REQUEST["base"];
-} else {
-    $_REQUEST["base"]="marc";
-}
-  
-if (isset($_REQUEST['prefijo']) && isset($_REQUEST['Sub_Expresion'])) {
-    $Expresion=$_REQUEST['prefijo'].$_REQUEST['Sub_Expresion'];
-} else {
-    $Expresion="TW_$";
-}
-
-$busqueda=$Expresion;
-*/
-
-$facetas="S";
-
-
-if (isset($facetas) and $facetas=="S" and (!isset($_REQUEST["prefijoindice"]) OR $_REQUEST["prefijoindice"]=="")){
-    $archivo="";
-if (file_exists($db_path."/opac_conf/".$_REQUEST["lang"]."/".$_REQUEST["base"]."_facetas.dat")){
-    $archivo=$db_path."/opac_conf/".$_REQUEST["lang"]."/".$_REQUEST["base"]."_facetas.dat";
-    }else{
-    if (file_exists($db_path."/opac_conf/".$_REQUEST["lang"]."/facetas.dat"))
-        $archivo=$db_path."/opac_conf/".$_REQUEST["lang"]."/"."facetas.dat";
+        if ( (isset($_REQUEST['base'])) &&  ($_REQUEST['base']!="") ) {
+        $db_facetas = $db_path . $_REQUEST['base'] . "/opac/" . $_REQUEST["lang"] . "/" . $_REQUEST['base'] . "_facetas.dat";
+        $bd_orig = $_REQUEST['base'];
+    }else {
+        $db_facetas="";
+        $bd_orig= $bd_list[$v[0]]["nombre"];
     }
 
-    if ($archivo!=""){
-        $fp=file($archivo);
-    if (count($fp)>0){
-?>
+
+        if ((file_exists($db_facetas)) && (isset($_REQUEST['base']))) {
+            $archivo = $db_facetas;
+        } else {
+            if (file_exists($db_path . "opac_conf/" . $_REQUEST["lang"] . "/facetas.dat")) {
+                $archivo = $db_path . "opac_conf/" . $_REQUEST["lang"] . "/" . "facetas.dat";
+            }
+        }
+    }
 
 
-            <?php
-            $fp = file($archivo);
-            foreach ($fp as $value_key) {
-                $value_key = trim($value_key);
-                if ($value_key != "") {
-                    $x = explode('|', $value_key);
-                    //echo " <li class='nav-item'><a class='nav-link' href='javascript:Facetas(\"$value_key\")'>" . $x[0];
-                    $IsisScript = "opac/buscar.xis";
-                    if ($Expresion == '$')
-                        $ex = $x[1];
-                    else
-                        $ex = $x[1] . " and " . $busqueda;
-                    if (isset($Expresion_col) and $Expresion_col != "") {
-                        $ex .= " and " . $Expresion_col;
-                    }
-                    if (isset($_REQUEST["base"]) and $_REQUEST["base"] != "")
-                        $base = $_REQUEST["base"];
-                    else
-                        $base = $primera_base;
-                    $query = "&base=$base&cipar=$db_path" . $actparfolder . "$base" . ".par&Expresion=" . urlencode($ex) . "&from=1&count=1&Opcion=buscar&lang=".$lang;
-                    //echo $query;
-                    $resultado = wxisLlamar($base, $query, $xWxis . $IsisScript);
-                    
-                    foreach ($resultado as $value) {
-                        $value = trim($value);
-                        if (trim($value) != "") {
-                            if (substr($value, 0, 8) == "[TOTAL:]") {
-                                $list_fac=" <li class='nav-item'><a class='nav-link' href='javascript:Facetas(\"$value_key\")'>" . $x[0];
-                                $list_fac.=" (" . substr($value, 8) . ")";
-                                $list_fac.="</a></li>";
-                                echo $list_fac;
-                                break;
-                            }
-                        }
-                    }
-                    
+    $conteudo = file($archivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($conteudo as $linha) {
+        list($cabecalho, $formato, $pref) = explode("|", $linha);
+
+        // Gera o cabeçalho <h4>
+        echo "<div class='faceta-box'>";
+        echo "<hr><h6>" . trim($cabecalho) . "</h6>";
+
+        $bd_ref = $bd_orig;
+
+        $arrHttp["base"] = $bd_ref;
+        $arrHttp["cipar"] = $bd_ref . ".par";
+
+        $expresion = construir_expresion();
+        $expresion = removeacentos($expresion);
+
+        //ECHO $expresion;
+
+        $arrHttp["Opcion"] = "buscar";
+        $Formato = trim($formato);
+
+        $query_param = "&cipar=" . $db_path . "par/" . $arrHttp["cipar"];
+        //$query_param.="&prefijo=".$prefixo;
+        $query_param .= "&Expresion=" . $expresion;
+        $query_param .= "&Opcion=" . $arrHttp["Opcion"];
+        $query_param .= "&base=" . $arrHttp["base"];
+        $query_param .= "&from=1";
+        $query_param .= "&Formato=" . $Formato;
+
+        $query = $query_param;
+
+
+        $IsisScript = $xWxis . "opac/facetas.xis";
+
+        include($ABCD_scripts_path . "central/common/wxis_llamar.php");
+
+        $ocurrencias = [];
+
+        foreach ($contenido as $value) {
+            $value_tratado = trim($value); // Remove espaços em branco do início e do fim
+
+            if (!empty($value_tratado)) { // Verifica se o valor não está vazio após o tratamento
+                if (isset($ocurrencias[$value_tratado])) {
+                    $ocurrencias[$value_tratado]++;
+                } else {
+                    $ocurrencias[$value_tratado] = 1;
                 }
             }
-            ?>
+        }
+
+        arsort($ocurrencias);
+        echo '<ul class="list-group">';
 
 
-<?php
 
-		}
-	}
+        foreach ($ocurrencias as $termo => $quantidade) {
+
+
+            $expresion = str_replace('(', '', $expresion);
+            $expresion = str_replace(')', '', $expresion);
+            $expresion = str_replace('+and+', ') and (', $expresion);
+            //$expresion = str_replace(' ', '+', $expresion);
+
+            //$faceta_atual = str_replace(' ', '+', $pref . $termo);
+            $faceta_atual = $pref . $termo;
+            $faceta_atual = removeacentos($faceta_atual);
+
+            // Determinar se o link da faceta deve ser negrito
+            $negrito = '';
+            if (strpos($expresion, $faceta_atual) !== false) {
+                $negrito = 'style="font-weight: bold;"';
+            }
+
+
+            echo '<li class="list-group-item">
+                <label>
+                    <a href="javascript:RefinF(\'' . $faceta_atual . '\', \'' . $expresion . '\')" ' . $negrito . '>+ ' . $termo . " (" . $quantidade . ")</a>
+                </label>
+              </li>";
+        }
+        echo '</ul>';
+        echo "</div>";
+    }
 }
 
 
-}//end facets
+if (function_exists('PresentarExpresion')) {
+    $resultado = PresentarExpresion($base); // Chama a função se ela existir
 
-
-//if (isset($_REQUEST['Sub_Expresion']))   {
 ?>
+    <h5><?php echo $msgstr["front_su_consulta"]; ?>:</h5>
+    <div id="termosAtivos" class="mb-3" data-link-inicial="<?php echo htmlspecialchars($link_logo); ?>">
+        <?php
+        $expOriginal = $resultado;
+        //$expOriginal = isset($_REQUEST['Expresion']) ? $_REQUEST['Expresion'] : '';
+        $expFormatada = str_replace('"', '', $expOriginal);
+        $termos = preg_split('/\s+and\s+/i', $expFormatada);
 
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel"><?php echo $msgstr["facetas"] ?></h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
+        foreach ($termos as $indice => $termo) {
+            $termoLimpo = strtolower(trim(preg_replace('/^[^_]*_/', '', $termo), " )("));
+            echo "<button type='button' class='btn btn-outline-primary btn-sm mr-1 mb-1 termo' onclick='removerTermo(\"" . htmlspecialchars($termo) . "\")'>";
+            echo removeacentos($termoLimpo);
+            echo " <span aria-hidden='true'>&times;</span></button>";
+        }
+        ?>
+    </div>
+    <br>
 
-<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-       <?php  facetas(); ?>
-        </ul>
-  </div>
-</div>
+    <button type="button" class="btn btn-outline-danger mt-3 ml-2" onclick="clearAndRedirect('<?php echo $link_logo; ?>')">Limpar Pesquisa</button>
+
+
+
+    <h4 class="mt-4">Refinar sua busca</h4>
+    <form id="facetasForm" method="GET" class="form-inline mt-3 mb-3" onsubmit="event.preventDefault(); processarTermosLivres();">
+        <input type="hidden" name="page" value="startsearch">
+
+
+        <?php $expresion = construir_expresion(); ?>
+
+        <input type="hidden" name="Expresion" id="Expresion" value="<?php echo htmlspecialchars($expresion); ?>">
+
+        <input type="hidden" name="Opcion" value="directa">
+        <?php if (isset($_REQUEST['base'])) { ?>
+            <input type="hidden" name="base" value="<?php echo $_REQUEST['base']; ?>">
+         <?php } ?>
+        <input type="hidden" name="lang" value="<?php echo $_REQUEST['lang']; ?>">
+        <?php if (isset($_REQUEST['indice_base'])) { ?>
+            <input type="hidden" name="indice_base" value="<?php echo $_REQUEST['indice_base']; ?>">
+        <?php } ?>
+        <input type="hidden" name="modo" value="1B">
+        <input type="hidden" name="resaltar" value="S">
+
+        <div class="form-group mr-2 mb-3">
+            <label for="termosLivres" class="mr-2">Termos livres:</label>
+            <input type="text" class="form-control" name="termosLivres" id="termosLivres" placeholder="Digite termos separados por espaço">
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar à busca</button>
+    </form>
 
 <?php
-//}
-?>
+    facetas();
+} else {
+    echo "";
+    // Ou então, logar o erro, lançar uma exceção, etc.
+}
 
+?>
