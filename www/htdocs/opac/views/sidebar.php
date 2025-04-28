02008b8ca7e5618b2b13ec669b0d3fbfd73ac3af
Roger C Guilherme <rogercguilherme@gmail.com>
2025-04-27 23:56:52 -0300

    Improves the sidebar for display on mobiles

--- www/htdocs/opac/views/sidebar.php
+++ www/htdocs/opac/views/sidebar.php
@@ -1,17 +1,12 @@
-<aside id="sidebar" class="col-3 d-md-block flex-column flex-shrink-0 p-3 sidebar collapse custom-sidebar">
-	
-<h5 class="card-title">
-
-	<?php
-
-	if (!isset($_REQUEST["existencias"]) or trim($_REQUEST["existencias"])=="" ){
-
-	include_once ('components/facets.php');
-	
-	include_once ($Web_Dir.'views/online_statment.php');
 
 
-		
-	}
-	?>
+<!-- Sidebar de facetas -->
+<aside id="sidebar" class="collapse d-md-block col-12 col-md-3 flex-shrink-0 p-3 custom-sidebar">
+	<h5 class="card-title">
+		<?php
+		if (!isset($_REQUEST["existencias"]) or trim($_REQUEST["existencias"]) == "") {
+			include_once('components/facets.php');
+			include_once($Web_Dir . 'views/online_statment.php');
+		}
+		?>
 </aside>
