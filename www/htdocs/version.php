be9eca7557c832f57199643962de8a41735785e9
Fred <64411779+fho4abcd@users.noreply.github.com>
2025-09-06 21:32:00 +0200

    Update version.php for v2.3.0

--- www/htdocs/version.php
+++ www/htdocs/version.php
@@ -2,5 +2,5 @@
 // This file defines the current version of the software.
 // It is the only place that needs to be changed with each new version.
 
-define('ABCD_VERSION', 'v2.2.0-beta-1');
-define('DATE_VERSION', '2025-08-24');
+define('ABCD_VERSION', 'v2.3.0');
+define('DATE_VERSION', '2025-06-09');
