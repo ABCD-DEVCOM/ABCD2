7c114caf6da72065ab4e48e349c2541d3935ea0e
Roger C Guilherme <rogercguilherme@gmail.com>
2025-09-13 00:46:49 -0300

    Improves the appearance of the configuration page.

--- www/htdocs/central/settings/conf_abcd.php
+++ www/htdocs/central/settings/conf_abcd.php
@@ -7,18 +7,13 @@
 20250305 fho4abcd Improve link to edit abcd.def
 */
 
-///////////////////////////////////////////////////////////////////////////////
-//
-//  MODIFICA LA CONFIGURACIÓN DE LA BASE DE DATOS
-//
-///////////////////////////////////////////////////////////////////////////////
 
 session_start();
-if (!isset($_SESSION["permiso"])){
-	header("Location: ../common/error_page.php") ;
+if (!isset($_SESSION["permiso"])) {
+	header("Location: ../common/error_page.php");
 }
-if (!isset($_SESSION["lang"]))  $_SESSION["lang"]="en";
-$lang=$_SESSION["lang"];
+if (!isset($_SESSION["lang"]))  $_SESSION["lang"] = "en";
+$lang = $_SESSION["lang"];
 include("../common/get_post.php");
 include("../config.php");
 include("../common/inc_nodb_lang.php");
@@ -29,10 +24,9 @@ include("../lang/dbadmin.php");
 include("../lang/prestamo.php");
 
 // VERIFICACION DE LA PERMISOLOTIA
-if (isset($_SESSION["permiso"]["CENTRAL_ALL"])){
-
-}else{
-	echo "<h2>".$msgstr["invalidright"]. " ".$base;
+if (isset($_SESSION["permiso"]["CENTRAL_ALL"])) {
+} else {
+	echo "<h2>" . $msgstr["invalidright"] . " " . $base;
 	die;
 }
 
@@ -43,103 +37,87 @@ include("../common/header.php");
 
 // INCLUSION DE LOS SCRIPTS
 ?>
+
 <body>
-<script language="JavaScript" type="text/javascript" src=../dataentry/js/lr_trim.js></script>
-<script language="JavaScript">
+	<script language="JavaScript" type="text/javascript" src=../dataentry/js/lr_trim.js></script>
+	<script language="JavaScript">
+		function Ejecutar(Script, Opcion) {
+			document.forma1.action = Script
+			if (Opcion != "")
+				document.forma1.Opcion.value = Opcion
+			document.forma1.submit()
 
-function Ejecutar(Script,Opcion){
-	document.forma1.action=Script
-	if (Opcion!="")
-		document.forma1.Opcion.value=Opcion
-	document.forma1.submit()
+		}
+	</script>
+	<?php
+	// ENCABEZAMIENTO DE LA PÁGINA
 
-}
-
-</script>
-<?php
-// ENCABEZAMIENTO DE LA PÁGINA
-
-include("../common/institutional_info.php");
-?>
-<div class=sectionInfo>
-    <div class=breadcrumb><?php echo $msgstr["configure"]. " ABCD";?>
-    </div>
-    <div class="actions">
-    <?php include "../common/inc_back.php"; ?>
+	include("../common/institutional_info.php");
+	?>
+	<div class="sectionInfo">
+		<div class="breadcrumb"><?php echo $msgstr["configure"] . " ABCD"; ?>
+		</div>
+		<div class="actions">
+			<?php include "../common/inc_back.php"; ?>
+		</div>
+		<div class="spacer">&#160;</div>
 	</div>
-	<div class="spacer">&#160;</div>
-</div>
-<?php
-$ayuda="admin.html";
-include "../common/inc_div-helper.php";
-?>
-<div class="middle form">
-<div class="formContent">
-<table width=400 align=center>
-	<tr>
-		<td>
-			<form name=update_base onSubmit="return false" method=post>
-			<input type=hidden name=Opcion value=update>
-			<input type=hidden name=type value="">
-			<input type=hidden name=modulo>
-			<?php if (isset($arrHttp["encabezado"])) {?>
-                <input type=hidden name=encabezado value=s>
-                <br>
-            <?php } ?>
+	<?php
+	$ayuda = "admin.html";
+	include "../common/inc_div-helper.php";
+	?>
+	<div class="middle homepage">
+		<div class="mainBox">
+			<div class="boxContent">
+				<div class="sectionTitle">
+					<h1><i class="fas fa-cogs"></i> <?php echo $msgstr["configure"] . " ABCD"; ?></h1>
+				</div>
 
-            <ul>
+				<div class="sectionButtons">
 
-            <?php if ($_SESSION["profile"]=="adm"){ ?>
-				<li>
-					<a href='javascript:Ejecutar("../settings/editar_abcd_def.php","abcd_styles")'>
-                    	<?php echo $msgstr["system_settings"]." (abcd.def)";?>
-                	</a>
-                </li>
-				<li>
-					<a href='Javascript:Ejecutar("../settings/databases_list.php","")'>
-                    	<?php echo $msgstr["dblist"];?>
-                    </a>
-                </li>
-				<li>
-					<a href='Javascript:Ejecutar("../settings/editar_correo_ini.php","")'>
-						<?php echo $msgstr["set_mail"];?>
-					</a>
-				</li>
-				<li>
-					<a href='opac/'>
-						OPAC
-					</a>
-				</li>	
-				
-				<?php
-				$script_abcd_stats = 'abcd_stats.php';
-				if (file_exists($script_abcd_stats)) {
-				?>					
-				<li>
-					<a href='Javascript:Ejecutar("../settings/abcd_stats.php","")'>
-						<?php echo $msgstr["s_e_overview"];?>
-					</a>
-				</li>
-				<?php
-				} 
-				?>
+					<?php if ($_SESSION["profile"] == "adm") { ?>
 
+						<a href='javascript:Ejecutar("../settings/editar_abcd_def.php","abcd_styles")' class="menuButton">
+							<span><i class="fas fa-file-alt" style="font-size: 2em; margin: 0 10px 0 -30px;"></i><strong><?php echo $msgstr["system_settings"] . " (abcd.def)"; ?></strong></span>
+						</a>
 
-			<?php } ?>
-            </ul>
-			</form>
-		</td>
-</table>
-<br>
-</div>
-</div>
+						<a href='Javascript:Ejecutar("../settings/databases_list.php","")' class="menuButton">
+							<span><i class="fas fa-database" style="font-size: 2em; margin: 0 10px 0 -30px;"></i><strong><?php echo $msgstr["dblist"]; ?></strong></span>
+						</a>
 
-<form name=forma1 method=post>
-<input type=hidden name=Opcion>
-</form>
+						<a href='Javascript:Ejecutar("../settings/editar_correo_ini.php","")' class="menuButton">
+							<span><i class="fas fa-envelope" style="font-size: 2em; margin: 0 10px 0 -30px;"></i><strong><?php echo $msgstr["set_mail"]; ?></strong></span>
+						</a>
 
-<?php
-// PIE DE PÁGINA
-include("../common/footer.php");
-?>
+						<a href='opac/' class="menuButton">
+							<span><i class="fas fa-globe" style="font-size: 2em; margin: 0 10px 0 -30px;"></i><strong>OPAC</strong></span>
+						</a>
 
+						<?php
+						$script_abcd_stats = 'abcd_stats.php';
+						if (file_exists($script_abcd_stats)) {
+						?>
+							<a href='Javascript:Ejecutar("../settings/abcd_stats.php","")' class="menuButton">
+								<span><i class="fas fa-chart-bar" style="font-size: 2em; margin: 0 10px 0 -30px;"></i><strong><?php echo $msgstr["s_e_overview"]; ?></strong></span>
+							</a>
+						<?php
+						}
+						?>
+
+					<?php } ?>
+
+				</div>
+				<div class="spacer">&#160;</div>
+			</div>
+		</div>
+	</div>
+
+	<form name=forma1 method=post>
+		<input type=hidden name=Opcion>
+		<input type=hidden name=encabezado value=s>
+	</form>
+
+	<?php
+	// PIE DE PÁGINA
+	include("../common/footer.php");
+	?>
