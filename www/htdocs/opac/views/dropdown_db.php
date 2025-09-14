82a1ba24991e6ed7331d8a5f419173b5c7260bbe
Roger C Guilherme <rogercguilherme@gmail.com>
2025-09-14 18:09:11 -0300

    Eliminates malfunctions

--- www/htdocs/opac/views/dropdown_db.php
+++ www/htdocs/opac/views/dropdown_db.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This script displays a dropdown with the list of available databases.
  * 20230312 rogercgui Created
@@ -6,74 +7,70 @@
 ?>
 
 <div class="dropdown">
-  <button class="btn btn-light dropdown-toggle w-100" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <?php echo $msgstr["front_catalog"];?>
-  </button>
+	<button class="btn btn-light dropdown-toggle w-100" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+		<?php echo $msgstr["front_catalog"]; ?>
+	</button>
 	<ul class="dropdown-menu w-100">
 
-<?php
-if (!isset($_REQUEST["existencias"]) or trim($_REQUEST["existencias"])=="" ){
+		<?php
+		if (!isset($_REQUEST["existencias"]) or trim($_REQUEST["existencias"]) == "") {
 
 
-    $primeravez="S";
-     if (isset($_REQUEST["modo"]) and $_REQUEST["modo"]!=""){
-}
+			$primeravez = "S";
+			if (isset($_REQUEST["modo"]) and $_REQUEST["modo"] != "") {
+			}
 
-	foreach ($bd_list as $key => $value){
-		$archivo=$db_path.$key."/opac/".$lang."/".$key."_colecciones.tab";
-		$ix=0;
-		$value_info="";
-		$home_link="*";
-		if (file_exists($db_path."opac_conf/".$lang."/".$key."_home.info")){
-			$home_info=file($db_path."opac_conf/".$lang."/".$key."_home.info");
-			foreach ($home_info as $value_info){
-				$value_info=trim($value_info);
-				if ($value_info!=""){
-					if (substr($value_info,0,6)=="[LINK]") $home_link=$value_info;
-					if (substr($value_info,0,6)=="[TEXT]") $home_link=$value_info;
-					if (substr($value_info,0,5)=="[MFN]")  $home_link="";
+			$num_db_list = count($bd_list);
+			$current_db_index = 0;
+
+			foreach ($bd_list as $key => $value) {
+				$archivo = $db_path . $key . "/opac/" . $lang . "/" . $key . "_colecciones.tab";
+				$ix = 0;
+				$value_info = "";
+				$home_link = "*";
+				if (file_exists($db_path . "opac_conf/" . $lang . "/" . $key . "_home.info")) {
+					$home_info = file($db_path . "opac_conf/" . $lang . "/" . $key . "_home.info");
+					foreach ($home_info as $value_info) {
+						$value_info = trim($value_info);
+						if ($value_info != "") {
+							if (substr($value_info, 0, 6) == "[LINK]") $home_link = $value_info;
+							if (substr($value_info, 0, 6) == "[TEXT]") $home_link = $value_info;
+							if (substr($value_info, 0, 5) == "[MFN]")  $home_link = "";
+						}
+					}
+					echo "**" . $value_info . "<br>";
 				}
-				echo "**".$value_info."<br>";
+				if (trim($value["nombre"]) != "") {
+					// Usando o cabeçalho de dropdown para o nome da base de dados
+					echo "<li><h5 class=\"dropdown-header\"><a class='dropdown-item' href='javascript:BuscarIntegrada(\"$key\",\"\",\"free\",\"\",\"\",\"\",\"\",\"\",\"\",\"$home_link\")'>" . $value["titulo"] . "</a></h5></li>\n";
+					if (file_exists($archivo)) {
+						$fp = file($archivo);
+						foreach ($fp as $colec) {
+							$colec = trim($colec);
+							if ($colec != "") {
+								$v = explode('|', $colec);
+								$ix = $ix + 1;
+								$col_expr = isset($v[2]) ? $v[2] . $v[0] : "";
+								if ($v[0] != '<>') {
+									if (isset($IndicePorColeccion) and $IndicePorColeccion == "Y")
+										$cipar = "_" . strtolower($v[0]);
+									else
+										$cipar = "";
+									echo "<li>";
+									echo "<a class='dropdown-item' href='javascript:BuscarIntegrada(\"$key\",\"1B\",\"free\",\"$col_expr\",\"$colec\",\"\",\"\",\"\",\"\",\"\")'>" . $v[1] . "</a>";
+									echo "</li>";
+								}
+							}
+						}
+					}
+				}
+
+				$current_db_index++;
+				if ($current_db_index < $num_db_list) {
+					echo "<li><hr class=\"dropdown-divider\"></li>\n";
+				}
 			}
 		}
-		if (trim($value["nombre"])!=""){
-			echo "<li><a class='dropdown-item' href='javascript:BuscarIntegrada(\"$key\",\"\",\"free\",\"\",\"\",\"\",\"\",\"\",\"\",\"$home_link\")'>";
-			echo "<strong>".$value["titulo"]."</strong></a></li>\n";
-	    	if (file_exists($archivo)){
-	    		$fp=file($archivo);
-				?>
-	    		<ul class="list-unstyled">
-				<?php
-	    		foreach ($fp as $colec){
-	          		$colec=trim($colec);
-	          		if ($colec!=""){
-	          			$v=explode('|',$colec);
-	          			$ix=$ix+1;
-	          			if ($v[0]!='<>'){
-							if (isset($IndicePorColeccion) and $IndicePorColeccion=="Y")
-								$cipar="_".strtolower($v[0]);
-							else
-								$cipar="";
-							echo "<li>";
-							//echo "<a class='dropdown-item' href='javascript:BuscarIntegrada(\"$key\",\"1B\",\"free\",\"\",\"$colec\",\"\",\"\",\"\",\"\",\"\")'>";
-							echo "<a href=\"buscar_integrada.php?base=".$key."&cipar=".$key.$cipar."&coleccion=".$colec."&Opcion=free\">";
-			          		echo $v[1]."</a></li>\n";
-	          			}else{
-		          				//echo "<li>".$v[1]."</i></label></a></li>\n";
-		        		}
-		          	}
-	          	}
-	          	echo "</ul>\n";
-				echo '<li><hr class="dropdown-divider"></li>';
-	    	}else{
-	    		//echo "</li>\n";
-	   		}
-	     }
-
-	}
-?>
-
-  </ul>
+		?>
+	</ul>
 </div>
-
-<?php } ?>
