cedfb9ce569a6cff99fc04c015cfdd08bd1fbf23
Roger C Guilherme <rogercguilherme@gmail.com>
2025-09-15 09:36:05 -0300

    Fixes a serious error in the databases.php file
    Fix the date in version.php

--- www/htdocs/version.php
+++ www/htdocs/version.php
@@ -3,4 +3,4 @@
 // It is the only place that needs to be changed with each new version.
 
 define('ABCD_VERSION', 'v2.3.1');
-define('DATE_VERSION', '2025-06-15');
+define('DATE_VERSION', '2025-09-15');
