35625ecb972dd83ef3c6d301c36fb591808be23d
Roger C Guilherme <rogercguilherme@gmail.com>
2025-09-16 18:24:08 -0300

    Updates the version as a milestone for the definitive implementation of Update Manager.

--- www/htdocs/version.php
+++ www/htdocs/version.php
@@ -2,5 +2,5 @@
 // This file defines the current version of the software.
 // It is the only place that needs to be changed with each new version.
 
-define('ABCD_VERSION', 'v2.3.3');
-define('DATE_VERSION', '2025-09-16');
+define('ABCD_VERSION', 'v2.3.4');
+define('DATE_VERSION', '2025-09-17');
